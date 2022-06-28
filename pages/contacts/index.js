f6bfc6405dbcb8ac306806b6fb0e1f5838c18fdf
= <=>
2022-06-28 14:13:02 +0400

    fetching_data

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,52 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Heading from "../../components/Heading";
 
-const Contacts = () => (
-  <>
-    <Head>
-      <title>Contacts</title>
-    </Head>
-    <Heading text="Contacts list:" />
-  </>
-);
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
 
-export default Contacts;
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { contacts: data },
+  }
+};
+
+const Contacts = ({ contacts }) => { 
+
+
+// const Contacts = () => { 
+
+//   const [contacts, setContacts] = useState(null);
+
+//   useEffect(()=>{
+//     const  fetchData = async () => {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//       const data = await response.json()
+//       setContacts(data)
+//     }
+//     fetchData()
+//   }, [])
+  
+
+  return (
+    <>
+      <Head>
+        <title>Contacts</title>
+      </Head>
+      <Heading text="Contacts list:" />
+      <ul>
+        {contacts && contacts.map(({ id, name, email }) => (
+          <li key={id}><strong>{name}</strong> ({email})</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Contacts; 
