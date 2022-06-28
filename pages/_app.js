d3e706c823dcea6dd8940998a7e0780a266c4e9a
= <=>
2022-06-28 12:31:32 +0400

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import Layout from "../components/Layout"
+import "../styles/globals.scss"
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
-    <Component {...pageProps} />
+    <main>
+      <Component {...pageProps} />
+    </main>
   </Layout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Heading from "../components/Heading"
+import styles from  "../styles/Home.module.scss"
 
-
-export default function Home() {
+const Home = () => {
   return (
+    <div className={styles.wrapper}>
     <Heading text="Hello World"/>
+  </div>
   )
 }
+
+export default Home
